f0972649156fc1b1fa96fd6c091ff3feeb2a7400
antonilol <49868160+antonilol@users.noreply.github.com>
2019-09-29 18:10:53 +0200

    Updates.

--- main.ts
+++ main.ts
@@ -1,19 +1,26 @@
 namespace noise {
-    function overflow(numb:number):number{
-        let output=0
-        let pre:number[]
+    function overflow(numb: number): number {
+        let output = 0
+        let pre: number[] = []
         for (let index = 0; index <= 31; index++) {
             if (numb >= 2 ** (31 - index)) {
                 numb += -1 * 2 ** (31 - index)
-                pre.push(1)                
+                pre.push(1)
             } else {
                 pre.push(0)
             }
         }
+let count=2**31
+        for (let v of pre) {
+            if (v){
+                output+=count
+            }
+count=count/2
+        }
 
         return output
     }
-    console.log(""+overflow(6))
+    console.log("" + overflow(6796209642065140602))
     export function noise2d(seed: number, x: number, y: number): number {
         return 0
     }
