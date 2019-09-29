ba322297f73eb15b0ceee682c6ac4ce9f99093e6
antonilol <49868160+antonilol@users.noreply.github.com>
2019-09-29 17:52:49 +0200

    Updates.

--- main.ts
+++ main.ts
@@ -1,11 +1,15 @@
-namespace noise{
-    export function noise2d(x:number,y:number):number{
+namespace noise {
+    function overflow(bigint:number):number{
+        
         return 0
     }
-    export function noise3d(x: number, y: number,z:number): number {
+    export function noise2d(seed: number, x: number, y: number): number {
         return 0
     }
-    export function noise4d(x: number, y: number,z:number,w:number): number {
+    export function noise3d(seed: number, x: number, y: number, z: number): number {
         return 0
     }
-} 
+    export function noise4d(seed: number, x: number, y: number, z: number, w: number): number {
+        return 0
+    }
+}
