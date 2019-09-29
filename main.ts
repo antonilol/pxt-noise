2e3ac44d858a62a742106fde2501f2509303c655
antonilol <49868160+antonilol@users.noreply.github.com>
2019-09-29 14:20:39 +0200

    Updates.

--- main.ts
+++ main.ts
@@ -1,7 +1,11 @@
-basic.showLeds(`
-    . . . . .
-    . # . # .
-    . . . . .
-    # . . . #
-    . # # # .
-    `);
+namespace noise{
+    export function noise2d(x:number,y:number):number{
+        return 0
+    }
+    export function noise3d(x: number, y: number,z:number): number {
+        return 0
+    }
+    export function noise4d(x: number, y: number,z:number,w:number): number {
+        return 0
+    }
+} 
